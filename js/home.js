8b23f4413332f7768df232ffa9d6ea6448c3c64b
Phat <22130206@st.hcmuaf.edu.vn>
2024-11-04 20:36:38 +0700

    update slider

---
+++ js/home.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function() {
+    const slider = document.querySelector(".slider");
+    const sliderMain = document.querySelector(".slider");
+    const sliderItems = document.querySelectorAll(".slider-item");
+    const nextBtn = document.querySelector(".slider-next");
+    const prevBtn = document.querySelector(".slider-prev");
+    const dotItem = document.querySelector(".slider-dot-item");
+
+    const imgWidth = sliderItems[0].offsetWidth;
+    const sliderLength = sliderItems.length;
+});
