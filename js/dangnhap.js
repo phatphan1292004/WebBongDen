accee98f5267afd09ddafe84f9b24773f3a4ee3c
CongLy18 <22130160@st.hcmuaf.edu.vn>
2024-11-06 21:12:53 +0700

    css

--- js/dangnhap.js
+++ js/dangnhap.js
@@ -1,10 +1,18 @@
 const formOpenBtn = document.querySelector(".login_btn"),
 formCloseBtn = document.querySelector(".form_close"),
-home = document.querySelector(".home")
+home = document.querySelector(".home"),
+formContainer = document.querySelector(".form_container"),
+signupBtn = document.querySelector("#signup"),
+loginBtn = document.querySelector("#login");
 
 
-formOpenBtn.addEventListener("click" , () => home.classList.add("show"))
-formOpenBtn.addEventListener("click" , () => home.classList.remove("show"))
+
+formOpenBtn.addEventListener("click" , () =>{
+    document.body.classList.toggle("show")})
+
+
+formCloseBtn.addEventListener("click" , () => {
+    home.classList.remove("show")})
 
 
 
