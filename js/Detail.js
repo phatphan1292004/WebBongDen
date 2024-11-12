968415bc0fddbef43430c301ea4a4fb794095d51
M2K1423 <164112701+M2K1423@users.noreply.github.com>
2024-11-13 00:02:15 +0700

    thêm boostrap

--- js/Detail.js
+++ js/Detail.js
@@ -81,14 +81,102 @@ function decreaseQuantity(){
             newPrice: "2,800,000",
             discount: 30
         }
-       
+       , {
+        name: "Đèn chùm bằng đồng trang trí phòng khách phong cách",
+        image1: "https://denhoamy.vn/upload/attachment/thumb/7301den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03443-01.jpg",
+        image2: "https://denhoamy.vn/upload/attachment/thumb/2376den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03438-02.jpg",
+        oldPrice: "3,000,000",
+        newPrice: "2,100,000",
+        discount: 30
+    },
+    {
+        name: "Đèn chùm bằng đồng trang trí phòng khách phong cách",
+        image1: "https://denhoamy.vn/upload/attachment/thumb/1300den-chum-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03427-04.jpg",
+        image2: "https://denhoamy.vn/upload/attachment/thumb/5225den-chum-pha-le-trang-tri-phong-khach-phong-cach-trung-hoa-dc03444-04.jpg",
+        oldPrice: "4,000,000",
+        newPrice: "2,800,000",
+        discount: 30
+    },
+    {
+        name: "Đèn chùm pha lê cao cấp",
+        image1: "https://denhoamy.vn/upload/attachment/thumb/7301den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03443-01.jpg",
+        image2: "https://denhoamy.vn/upload/attachment/thumb/5225den-chum-pha-le-trang-tri-phong-khach-phong-cach-trung-hoa-dc03444-04.jpg",
+        oldPrice: "5,000,000",
+        newPrice: "3,500,000",
+        discount: 30
+    },
+    {
+        name: "Đèn chùm pha lê sang trọng cho phòng khách",
+        image1: "https://denhoamy.vn/upload/attachment/thumb/7301den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03443-01.jpg",
+        image2: "https://denhoamy.vn/upload/attachment/thumb/7000den-chum-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03441-02.jpg",
+        oldPrice: "6,000,000",
+        newPrice: "4,200,000",
+        discount: 30
+    },
+    {
+        name: "Đèn chùm pha lê kiểu dáng hiện đại",
+        image1: "https://denhoamy.vn/upload/attachment/thumb/7301den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03443-01.jpg",
+        image2: "https://denhoamy.vn/upload/attachment/thumb/1300den-chum-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03427-04.jpg",
+        oldPrice: "7,000,000",
+        newPrice: "5,000,000",
+        discount: 30
+    },
+    {
+        name: "Đèn chùm thủy tinh trang trí phòng khách",
+        image1: "https://denhoamy.vn/upload/attachment/thumb/1300den-chum-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03427-04.jpg",
+        image2: "https://denhoamy.vn/upload/attachment/thumb/2376den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03438-02.jpg",
+        oldPrice: "4,500,000",
+        newPrice: "3,000,000",
+        discount: 33
+    },
+    {
+        name: "Đèn chùm đồng trang trí phòng khách",
+        image1: "https://denhoamy.vn/upload/attachment/thumb/7301den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03443-01.jpg",
+        image2: "https://denhoamy.vn/upload/attachment/thumb/2376den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03438-02.jpg",
+        oldPrice: "2,500,000",
+        newPrice: "1,800,000",
+        discount: 28
+    },
+    {
+        name: "Đèn chùm pha lê cho phòng khách sang trọng",
+        image1: "https://denhoamy.vn/upload/attachment/thumb/1300den-chum-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03427-04.jpg",
+        image2: "https://denhoamy.vn/upload/attachment/thumb/7000den-chum-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03441-02.jpg",
+        oldPrice: "5,500,000",
+        newPrice: "3,800,000",
+        discount: 31
+    },
+    {
+        name: "Đèn chùm cao cấp cho phòng khách hiện đại",
+        image1: "https://denhoamy.vn/upload/attachment/thumb/7301den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03443-01.jpg",
+        image2: "https://denhoamy.vn/upload/attachment/thumb/7000den-chum-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03441-02.jpg",
+        oldPrice: "7,200,000",
+        newPrice: "5,200,000",
+        discount: 28
+    },
+    {
+        name: "Đèn chùm thủy tinh hiện đại cho phòng khách",
+        image1: "https://denhoamy.vn/upload/attachment/thumb/7301den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03443-01.jpg",
+        image2: "https://denhoamy.vn/upload/attachment/thumb/5225den-chum-pha-le-trang-tri-phong-khach-phong-cach-trung-hoa-dc03444-04.jpg",
+        oldPrice: "6,500,000",
+        newPrice: "4,500,000",
+        discount: 31
+    },
+    {
+        name: "Đèn chùm đồng trang trí phòng khách sang trọng",
+        image1: "https://denhoamy.vn/upload/attachment/thumb/7301den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03443-01.jpg",
+        image2: "https://denhoamy.vn/upload/attachment/thumb/2376den-chum-thong-tang-bang-dong-trang-tri-phong-khach-phong-cach-trung-hoa-dc03438-02.jpg",
+        oldPrice: "5,000,000",
+        newPrice: "3,500,000",
+        discount: 30
+    },
     ];
+    
 
     document.addEventListener("DOMContentLoaded", function() {
         let container = document.getElementById('product-container');
         products.forEach((product, index) => {
             container.innerHTML += `
-                <div class="col-6 col-md-4 col-lg-3">
+                <div class="col-6 col-md-4 col-lg-3 mb-4">
                     <div class="single-product-wrapper">
                         <div class="product-img">
                             <img src="${product.image1}" alt="Product Image" onerror="this.src='https://via.placeholder.com/150';">
