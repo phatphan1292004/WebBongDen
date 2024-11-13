632b15baeb6cb13f4a16a9b5da2701c818c8f3ea
Phat <22130206@st.hcmuaf.edu.vn>
2024-11-14 00:22:36 +0700

    fix user-info page

--- js/login.js
+++ js/login.js
@@ -1,50 +1,31 @@
-// element của trang
-const formLogin = document.getElementById("formLogin");
-
- // lắng nghe sự kiện submit
-formLogin.addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    //validate dữ liệu đầu vào
-
-
-    //lấy dữ liệu từ local về
-    const userLocal = JSON.parse(localStorage.getItem("users"))
-
-    //tìm kiếm userName và mật khẩu người có tồn tại trên local ?
-
-
-    //nếu có thì đăng nhập thành ocong và chuyển hướng về trang chủ
-
-
-    // nếu không thì thông báo cho người dùng nhập lại dữ liệu
-
-    // Kiểm tra nếu tên hoặc mật khẩu để trống
-    if (name === "" || password === "") {
-        document.getElementById('loginMessage').innerText = "Please fill in both fields.";
-        return;
-    }
-
-    // Kiểm tra giả lập (ở đây bạn có thể thực hiện kiểm tra thật nếu có server)
-    if (name === "user" && password === "password") {
-        document.getElementById("loginMessage").innerText = "Login successful!";
-        document.getElementById("loginMessage").style.color = "green";
-    } else {
-        document.getElementById("loginMessage").innerText = "Incorrect name or password.";
-        document.getElementById("loginMessage").style.color = "red";
-    }
-});
-function responseMassage(){
+document.addEventListener('DOMContentLoaded', function() {
+function responseMassage() {
     const email = document.getElementById("reset-email").value;
-    document.getElementById("responseMessage").innerText = "mặt khẩu đã được gửi tới ${email}";
-}
-function showForgotPassword() {
+    document.getElementById("responseMessage").innerText =
+      "mặt khẩu đã được gửi tới ${email}";
+  }
+  function showForgotPassword() {
     document.getElementById("login-form").style.display = "none";
     document.getElementById("forgot-password-form").style.display = "block";
-}
-
-function showLogin() {
+  }
+  
+  function showLogin() {
     document.getElementById("forgot-password-form").style.display = "none";
     document.getElementById("login-form").style.display = "block";
-}
-
+  }
+  
+  // Chuyển tới trang Admin khi nhập tk mk của admin
+  document
+    .getElementById("login-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const username = document.getElementById("cus-account").value;
+      const password = document.getElementById("cus-password").value;
+  
+      if (username === "admin123" && password === "admin123") {
+        window.location.href = "  admin.html";
+      } else {
+        alert("Tài khoản hoặc mật khẩu không chính xác.");
+      }
+    });  
+});
