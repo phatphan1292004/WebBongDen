43f941b77c8b9dc2b07ac5745ffc9a5abb812108
Phat <22130206@st.hcmuaf.edu.vn>
2024-12-14 19:42:32 +0700

    tao cay thu muc

---
+++ WebBongDen/src/main/java/com/example/webbongden/controller/controllerUser/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.webbongden.controller.controllerUser;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "HomeController", value = "/Home")
+public class HomeController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
---
+++ WebBongDen/src/main/java/com/example/webbongden/controller/controllerUser/LoginController.java
@@ -0,0 +1,20 @@
+package com.example.webbongden.controller.controllerUser;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginController", value = "/Login")
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    request.getRequestDispatcher("/WEB-INF/user/Login.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
